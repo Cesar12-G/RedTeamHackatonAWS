c7aa22a34462366b98ff2e345aeac6723f38d234
crodriguez12 <crodriguez@unifiedcommunications.com>
2022-09-07 00:03:25 -0500

    Combining Django and React, creating index page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from SiteContent import views as SiteContentViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', SiteContentViews.react_app)
 ]
