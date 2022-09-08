7ffed00dd8ba9c6155a30c94d17ef0914905e4ba
eritsg <erit@torodigital.com.mx>
2022-09-07 21:57:57 -0500

    Properties api endpoints, serializer, Create, Read

--- backend/properties/models.py
+++ backend/properties/models.py
@@ -21,7 +21,7 @@ class Property(models.Model):
     updated = models.DateTimeField(verbose_name="Updated date", auto_now=True)
 
 class Location(models.Model):
-    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property", on_delete=models.CASCADE)
+    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property_location", on_delete=models.CASCADE)
     lat         = models.BooleanField(default=True, verbose_name="Lat")
     lng         = models.BooleanField(default=True, verbose_name="Lng")
 
@@ -29,21 +29,21 @@ class Location(models.Model):
     updated = models.DateTimeField(verbose_name="Updated date", auto_now=True)
 
 class Price(models.Model):
-    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property", on_delete=models.CASCADE)
+    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property_price", on_delete=models.CASCADE)
     price       = models.BooleanField(default=True, verbose_name="Price")
 
     created = models.DateTimeField(verbose_name="Created date",auto_now_add=True)
     updated = models.DateTimeField(verbose_name="Updated date", auto_now=True)
 
 class Appraisal(models.Model):
-    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property", on_delete=models.CASCADE)
+    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property_appraisal", on_delete=models.CASCADE)
     price       = models.BooleanField(default=True, verbose_name="Price")
 
     created = models.DateTimeField(verbose_name="Created date",auto_now_add=True)
     updated = models.DateTimeField(verbose_name="Updated date", auto_now=True)
 
 class Contact(models.Model):
-    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property", on_delete=models.CASCADE)
+    property    = models.ForeignKey(Property, verbose_name="Property", related_name="property_contact", on_delete=models.CASCADE)
     name        = models.CharField(max_length=50, verbose_name="Name")
     last_name   = models.CharField(max_length=50, verbose_name="Last Name")
     type        = models.CharField(max_length=50, verbose_name="Type")
--- backend/properties/views.py
+++ backend/properties/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import PropertySerializer
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Property
+
+# API REST
+class PropertyView(APIView):
+    
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, id = 0):
+        
+        if(id != 0):
+            propiedades   = list(Property.objects.filter(id=id).values())
+            serializer  = PropertySerializer(propiedades, many=True)
+            if len(propiedades) > 0:
+                datos = {
+                    'status'    :"Success",
+                    'message'   :"Property found",
+                    'property'  :serializer.data[0]
+                }
+            else:
+                datos = {
+                    'status'    :"Error",
+                    'message'   :"Property not found"
+                }
+        else:
+            propiedades   = list(Property.objects.values())
+            serializer  = PropertySerializer(propiedades, many=True)
+            if len(propiedades) > 0:
+                datos = {
+                    'status'    :"Success",
+                    'message'   :"Properties found",
+                    'found'     :len(propiedades),
+                    'properties':serializer.data
+                }
+            else:
+                datos = {
+                    'status'    :"Error",
+                    'message'   :"Properties not found"
+                }
+        
+        return Response(datos)
+    
+    def post(self, request):
+        
+        serializer  = PropertySerializer(data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                'status'    :"Success",
+                'message'   :'Property created',
+                'property'  :serializer.data
+            }
+        else:
+            response = {
+                'status'    :"Error",
+                'message'   :'Property not created'
+            }
+
+        return Response(response)
