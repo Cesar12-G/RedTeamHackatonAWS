b1e0dc0f6bb808789aa91a81fc4c06b1f652c09f
eritsg <erit@torodigital.com.mx>
2022-09-10 03:22:08 -0500

    Auth user model changed to custom

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,15 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # REST
     'corsheaders',
     'rest_framework',
+    # APPS
     'core',
+    'account',
     'SiteContent',
     'properties'
 ]
 
+AUTH_USER_MODEL = "account.User"
+
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -128,6 +133,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':    timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME':   timedelta(hours=8),
+    'ROTATE_REFRESH_TOKENS':    False,
+    'AUTH_HEADER_TYPES':        ('Bearer', ),
+    'AUTH_TOKEN_CLASSES':       ('rest_framework_simplejwt.tokens.AccessToken', ),
+}
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
